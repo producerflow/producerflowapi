fba348e4e3e912f341733e0689bb6fa23fb7468d
jferrl <jorge.ferrero@agentero.com>
2026-01-12 19:46:04 +0100

    Add proto-google-common-protos dependency
    Included the 'com.google.api.grpc:proto-google-common-protos' library to provide common protobuf types such as TimeOfDay, Date, and DayOfWeek.

--- gen/kotlin/build.gradle.kts
+++ gen/kotlin/build.gradle.kts
@@ -32,6 +32,9 @@ dependencies {
     // Protobuf runtime (standard Java for server usage)
     api("com.google.protobuf:protobuf-java:4.28.3")
 
+    // Google common protos (TimeOfDay, Date, DayOfWeek, etc.)
+    api("com.google.api.grpc:proto-google-common-protos:2.49.0")
+
     // OkHttp for HTTP transport
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
